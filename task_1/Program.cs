bb9ee2d58a523c5df7ca493bf1cb982f72a006e7
Ekaterina <kat.bommon@gmail.com>
2023-01-25 10:20:01 +0300

    Exponentiation number

---
+++ task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */ 
+
+int GetNumbers(string messange)
+{
+    Console.Write(messange);
+    int result=0;
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(),out result)) break;
+        else Console.Write("Введите число, чтобы продолжить -> ");
+    }
+    return result;
+}
+void ExponentiationNumber(int a , int b)
+{ 
+   Console.Write($"Ввозведение числа {a} в степень {b} -> {Math.Pow(a,b)}");
+}
+ExponentiationNumber(GetNumbers("Введите число-> "), GetNumbers("Введите степень ->"));
