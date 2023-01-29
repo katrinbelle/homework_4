a7581f65307146a9e18be1251f6271547bb85ad5
Ekaterina <kat.bommon@gmail.com>
2023-01-29 12:37:00 +0300

    Array out is Random

---
+++ task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+ int[] GetArray ()
+ {
+    int[] array = new int[8];
+    Random rnd=new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=rnd.Next(0,100);
+    }
+return array;
+ }
+
+ void OutArray(int[] arr)
+ { string mark=",";
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    { if(i==arr.Length-1) mark="]";
+        Console.Write(arr[i]+mark);
+    }
+ }
+ OutArray(GetArray());
